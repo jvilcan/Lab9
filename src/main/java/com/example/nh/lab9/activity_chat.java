81b25253af32a110775a27fd2b98426a2ace2e5f
Javier <jvilcan@gmail.com>
2018-11-23 21:00:43 -0500

    Second Version

--- src/main/java/com/example/nh/lab9/activity_chat.java
+++ src/main/java/com/example/nh/lab9/activity_chat.java
@@ -2,6 +2,7 @@ package com.example.nh.lab9;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class activity_chat extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class activity_chat extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        Toast.makeText(this,"Bienvenido al Chat Firebase", Toast.LENGTH_LONG).show();
     }
 }
